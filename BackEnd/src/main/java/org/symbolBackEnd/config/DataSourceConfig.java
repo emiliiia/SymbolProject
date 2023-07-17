28a6724dceb929d7408214708aa7aa204bc794fe
emiliia drahomyretska <90069258+emiliiia@users.noreply.github.com>
2023-07-17 14:03:07 +0300

    configuration

---
+++ BackEnd/src/main/java/org/symbolBackEnd/config/DataSourceConfig.java
@@ -0,0 +1,32 @@
+package org.symbolBackEnd.config;
+/*
+  @author emilia
+  @project SymbolProject
+  @class DataSourceConfig
+  @version 1.0.0
+  @since 17.07.2023 - 14:01
+*/
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+    @Bean
+    public DataSource getDataSource(@Value("${spring.datasource.url}") String url,
+                                    @Value("${spring.datasource.driverClassName}") String driverClassName,
+                                    @Value("${spring.datasource.username}") String username,
+                                    @Value("${spring.datasource.password}") String password
+    ) {
+        DataSourceBuilder<?> dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName(driverClassName);
+        dataSourceBuilder.url(url);
+        dataSourceBuilder.username(username);
+        dataSourceBuilder.password(password);
+        return dataSourceBuilder.build();
+    }
+}
---
+++ BackEnd/src/main/java/org/symbolBackEnd/config/JpaConfig.java
@@ -0,0 +1,17 @@
+package org.symbolBackEnd.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+/*
+  @author emilia
+  @project SymbolProject
+  @class JpaConfig
+  @version 1.0.0
+  @since 17.07.2023 - 14:02
+*/
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
