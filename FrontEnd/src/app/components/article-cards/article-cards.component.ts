56df3d39e8a6a287949c44916f5a84a118c47e26
emiliia drahomyretska <90069258+emiliiia@users.noreply.github.com>
2023-09-09 13:00:15 +0300

    articleCardPageFrontEnd

---
+++ FrontEnd/src/app/components/article-cards/article-cards.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-article-cards',
+  templateUrl: './article-cards.component.html',
+  styleUrls: ['./article-cards.component.css']
+})
+export class ArticleCardsComponent {
+
+}
