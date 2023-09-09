00b72913c93337583675db5daaf60ec4c1e817f0
emiliia drahomyretska <90069258+emiliiia@users.noreply.github.com>
2023-09-09 13:02:18 +0300

    introductionSectionPageFrontEnd

---
+++ FrontEnd/src/app/components/introduction-section/introduction-section.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-introduction-section',
+  templateUrl: './introduction-section.component.html',
+  styleUrls: ['./introduction-section.component.css']
+})
+export class IntroductionSectionComponent {
+
+}
