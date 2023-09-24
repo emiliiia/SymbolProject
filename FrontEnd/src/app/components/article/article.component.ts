5895e8666024d1a6bb36ef6da85736279a8a394a
emiliia drahomyretska <90069258+emiliiia@users.noreply.github.com>
2023-09-24 20:40:30 +0300

    articleFrontEndUpdate

--- FrontEnd/src/app/components/article/article.component.ts
+++ FrontEnd/src/app/components/article/article.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Post} from "../../models/post";
+import {Search} from "../../models/search";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
   styleUrls: ['./article.component.css']
 })
-export class ArticleComponent {
+export class ArticleComponent implements OnInit {
+  @Input() post!: Post;
+  @Input() postId!: string;
 
+  @Output() onEdit = new EventEmitter<boolean>();
+
+  searchParameter = new Search("createdAt", "DESC", 0,2, {postId:""})
+
+  postImage: string = "assets/images/image";
+
+
+  constructor(private router: Router) {
+  }
+  ngOnInit(): void {
+    this.searchParameter.searchPattern.postId = this.postId
+  }
+
+  onErrorPostImage(event: any) {
+    event.target.src = 'assets/imagees/default.jpg';
+  }
+
+  public enableEditMode() {
+    this.onEdit.emit(true);
+  }
 }
