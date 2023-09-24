cbc420deb3afcef4eefb346d7093517ff2d13624
emiliia drahomyretska <90069258+emiliiia@users.noreply.github.com>
2023-09-24 20:43:41 +0300

    PhotoSliderFrontEnd

--- FrontEnd/src/app/components/photo-slider/photo-slider.component.ts
+++ FrontEnd/src/app/components/photo-slider/photo-slider.component.ts
@@ -8,6 +8,11 @@ import {
   timer,
 } from 'rxjs';
 import { SlideInterface} from "../../directives/slide-interface";
+import {Page} from "../../models/pages";
+import {Search} from "../../models/search";
+import {MatSort, Sort} from "@angular/material/sort";
+import {PostService} from "../../services/post.service";
+import {Router} from "@angular/router";
 
 
 @Component({
@@ -15,14 +20,189 @@ import { SlideInterface} from "../../directives/slide-interface";
   templateUrl: './photo-slider.component.html',
   styleUrls: ['./photo-slider.component.css']
 })
-export class PhotoSliderComponent implements OnInit, OnDestroy {
-  @Input() slides: SlideInterface[] = [];
+export class PhotoSliderComponent implements OnInit {
+  page: Page = new Page([], 0);
+  searchParameter!: Search;
+  postImage: string = "assets/images/image";
+  keyword = 'name';
+  role!:any;
+  sort:Sort = new MatSort()
+
+
+  currentIndex: number = 0;
+  timeoutId?: number;
+
+  apply() {
+    this.searchParameter.page = 0
+    this.search()
+  }
+
+
+  changePage(page: number) {
+    this.searchParameter.page = page;
+    this.postService.setSearchPostParameter(this.searchParameter)
+    this.search()
+  }
+
+  setSorted(event: any) {
+    this.searchParameter.sortField = event.active
+    if (event.direction != "") {
+      this.searchParameter.sortDirection = event.direction.toUpperCase()
+    }
+    this.search()
+  }
+
+  getFirst() {
+    this.searchParameter.page = 0;
+    this.search()
+  }
+
+  getLast() {
+    if (this.page.totalItem / this.searchParameter.page < 0) {
+      this.searchParameter.page = 0;
+    } else {
+      this.searchParameter.page = Math.floor(this.page.totalItem / this.searchParameter.pageSize);
+    }
+    this.search()
+  }
+
+  constructor(private postService: PostService,
+              private router: Router) {
+  }
+
+  search() {
+    this.postService.getPostPage().subscribe((page: Page) => {
+      this.page = page
+    });
+  }
+
+
+  ngOnInit() {
+    this.resetTimer();
+    this.search();
+    this.searchParameter = this.postService.getSearchPostParameter();
+    this.sort.active = this.searchParameter.sortField;
+    this.sort.direction = this.searchParameter.sortDirection.toLowerCase() != "asc"?"desc":"asc";
+  }
+
+  ngOnDestroy() {
+    window.clearTimeout(this.timeoutId);
+  }
+  resetTimer() {
+    if (this.timeoutId) {
+      window.clearTimeout(this.timeoutId);
+    }
+    this.timeoutId = window.setTimeout(() => this.goToNext(), 3000);
+  }
+
+  onErrorPostImage(event: any) {
+    event.target.src = 'assets/images/default.png';
+  }
+
+  goToPrevious(): void {
+    const isFirstSlide = this.currentIndex === 0;
+    const newIndex = isFirstSlide
+      ? this.page.content.length - 1
+      : this.currentIndex - 1;
+
+    this.resetTimer();
+    this.currentIndex = newIndex;
+  }
+
+  goToNext(): void {
+    const isLastSlide = this.currentIndex === this.page.content.length - 1;
+    const newIndex = isLastSlide ? 0 : this.currentIndex + 1;
+
+    this.resetTimer();
+    this.currentIndex = newIndex;
+  }
+
+  goToSlide(slideIndex: number): void {
+    this.resetTimer();
+    this.currentIndex = slideIndex;
+  }
+
+  getCurrentSlideUrl() {
+    return `url('./assets/images/image17.jpg')`;
+  }
+
+  getCurrentSlideTitle() {
+    return this.page.content[this.currentIndex].title;
+  }
+
+  getCurrentSlideId() {
+    return 1;
+  }
+
+  getCurrentSlideDate() {
+    return this.page.content[this.currentIndex].publishedAt;
+  }
+}
+/*  @Input() slides: SlideInterface[] = [];
+  page: Page = new Page([], 0);
+  searchParameter!: Search;
+  postImage: string = "assets/images/image";
+  keyword = 'name';
+  role!:any;
+  sort:Sort = new MatSort()
+
+  apply() {
+    this.searchParameter.page = 0
+    this.search()
+  }
+
+
+  changePage(page: number) {
+    this.searchParameter.page = page;
+    this.postService.setSearchPostParameter(this.searchParameter)
+    this.search()
+  }
+
+  setSorted(event: any) {
+    this.searchParameter.sortField = event.active
+    if (event.direction != "") {
+      this.searchParameter.sortDirection = event.direction.toUpperCase()
+    }
+    this.search()
+  }
+
+  getFirst() {
+    this.searchParameter.page = 0;
+    this.search()
+  }
+
+  getLast() {
+    if (this.page.totalItem / this.searchParameter.page < 0) {
+      this.searchParameter.page = 0;
+    } else {
+      this.searchParameter.page = Math.floor(this.page.totalItem / this.searchParameter.pageSize);
+    }
+    this.search()
+  }
+
+  constructor(private postService: PostService,
+              private router: Router) {
+  }
+
+  search() {
+    this.postService.getPostPage().subscribe((page: Page) => {
+      this.page = page
+    });
+  }
+
+  onErrorPostImage(event: any) {
+    event.target.src = 'assets/images/default.png';
+  }
 
   currentIndex: number = 0;
   timeoutId?: number;
 
   ngOnInit(): void {
     this.resetTimer();
+    this.search();
+    this.searchParameter = this.postService.getSearchPostParameter();
+    this.sort.active = this.searchParameter.sortField;
+    this.sort.direction = this.searchParameter.sortDirection.toLowerCase() != "asc"?"desc":"asc";
   }
   ngOnDestroy() {
     window.clearTimeout(this.timeoutId);
@@ -69,3 +249,4 @@ export class PhotoSliderComponent implements OnInit, OnDestroy {
     return this.slides[this.currentIndex].date;
   }
 }
+*/
