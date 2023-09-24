a5fbd1ba3635efe5a2a19395ec6c94b771a1ded7
emiliia drahomyretska <90069258+emiliiia@users.noreply.github.com>
2023-09-24 20:44:06 +0300

    appUpdateFrontEnd

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -5,6 +5,10 @@ import {HomeComponent} from "./components/home/home.component";
 import {ArticlesComponent} from "./components/articles/articles.component";
 import {ArticleComponent} from "./components/article/article.component";
 import {SupportComponent} from "./components/support/support.component";
+import {ArticleForwardingComponent} from "./components/article-forwarding/article-forwarding.component";
+import {CreateArticleComponent} from "./components/create-article/create-article.component";
+import {RulesComponent} from "./components/rules/rules.component";
+import {AboutUsComponent} from "./components/about-us/about-us.component";
 
 export interface FileHandle {
   file: File;
@@ -14,7 +18,11 @@ export interface FileHandle {
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'articles', component: ArticlesComponent},
+  {path: 'articles/:id', component: ArticleForwardingComponent},
+  {path: 'aboutUs', component: AboutUsComponent},
   {path: 'article', component: ArticleComponent},
+  {path: 'createArticle', component: CreateArticleComponent},
+  {path: 'rules', component: RulesComponent},
   {path: 'support', component: SupportComponent},
   {path: '**', redirectTo: '/404'}
 ];
--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -5,7 +5,9 @@ import {AppRoutingModule} from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import {NgxPaginationModule} from 'ngx-pagination';
 import { PhotoSliderComponent } from './components/photo-slider/photo-slider.component';
 import { DirectionsSliderComponent } from './components/directions-slider/directions-slider.component';
 import {OwlModule} from "ngx-owl-carousel";
@@ -14,9 +16,15 @@ import { ArticlesComponent } from './components/articles/articles.component';
 import { SkillsComponent } from './components/skills/skills.component';
 import { AboutUsComponent } from './components/about-us/about-us.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { IntroductionSectionComponent } from './components/introduction-section/introduction-section.component';
 import { ArticleComponent } from './components/article/article.component';
 import { SupportComponent } from './components/support/support.component';
+import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+import {MatSortModule} from "@angular/material/sort";
+import { ArticleForwardingComponent } from './components/article-forwarding/article-forwarding.component';
+import { CreateArticleComponent } from './components/create-article/create-article.component';
+import {AngularEditorModule} from "@kolkov/angular-editor";
+import { RulesComponent } from './components/rules/rules.component';
 
 @NgModule({
   declarations: [
@@ -30,15 +38,24 @@ import { SupportComponent } from './components/support/support.component';
     SkillsComponent,
     AboutUsComponent,
     FooterComponent,
-    IntroductionSectionComponent,
     ArticleComponent,
     SupportComponent,
+    ArticleForwardingComponent,
+    CreateArticleComponent,
+    RulesComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    OwlModule
-  ],
+    imports: [
+        BrowserAnimationsModule,
+        BrowserModule,
+        AppRoutingModule,
+        OwlModule,
+        FormsModule,
+        HttpClientModule,
+        MatSortModule,
+        NgxPaginationModule,
+        AngularEditorModule,
+        // Add this line
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
